0494bb8c15fae4044e927e3d276df022fe9d45af
S_K_Tiger <55139409+s-k-tiger@users.noreply.github.com>
2020-05-13 14:19:41 +0200

    Updated 'biopython_use.py': Put the main in a function.
    Signed-off-by: s-k-tiger <lars.weijenborg@gmail.com>

--- static/biopython_use.py
+++ static/biopython_use.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 # coding=utf-8
 """
-TEMP file, for figuring shit out.
+For doing blasting.
 """
-import time  # https://www.ncbi.nlm.nih.gov/books/NBK25497/#chapter2.Usage_Guidelines_and_Requiremen
-
 # Bio = Biopython but Pycharm doesn't know.
 # noinspection PyPackageRequirements
 from Bio import SeqIO
@@ -12,28 +10,36 @@ from Bio import SeqIO
 from Bio.Blast import NCBIXML
 # noinspection PyPackageRequirements
 from Bio.Blast.NCBIWWW import qblast
-
-
 # print(help(qblast))
 
 
-def data_printer(blast_record, e_value_thresh=0.04):
+def data_printer(blast_record, e_value_thresh=0.04, line_len=80):
     # noinspection SpellCheckingInspection
     """
-            :param blast_record: NCBIXML.parse(result_handle)
-            :param e_value_thresh:
-            :return: None
+            :param line_len: The max length of the sequence lines. (Min 4)
+            :param blast_record: NCBIXML.parse(result_handle).
+            :param e_value_thresh: Treshold of e_value above which the results are not printed.
+            :return: None.
             """
-    for alignment in blast_record.alignments:
-        for hsp in alignment.hsps:
-            if hsp.expect < e_value_thresh:
-                print("****Alignment****")
-                print("sequence:", alignment.title)
-                print("length:", alignment.length)
-                print("e value:", hsp.expect)
-                print(hsp.query[0:75] + "...")
-                print(hsp.match[0:75] + "...")
-                print(hsp.sbjct[0:75] + "...")
+    if line_len > 3:
+        line_len -= 3
+    else:
+        print("Error in 'data_printer()': param 'line_len' is too low. (min 4)")
+        return
+    for record in blast_record:
+        for alignment in record.alignments:
+            for hsp in alignment.hsps:
+                if hsp.expect < e_value_thresh:
+                    print("****Alignment****")
+                    print("sequence:", alignment.title)
+                    print("length:", alignment.length)
+                    print("e value:", hsp.expect)
+                    if len(hsp.query) > line_len:
+                        print(hsp.query[0:line_len] + "...")
+                        print(hsp.match[0:line_len] + "...")
+                        print(hsp.sbjct[0:line_len] + "...\n")
+                    else:
+                        print(f'{hsp.query}\n{hsp.match}\n{hsp.sbjct}\n')
     return
 
 
@@ -41,7 +47,7 @@ def bio_blaster(
         input_file: str, file_format: str, output_file: str, index: str = None,
         program='blastn', database='nt', gi_format=True, size=10
 ):
-    """Blasting sort of automated. TODO: testing
+    """Blasting sort of automated.
     :param input_file: The file path which file contains a/the sequence that is to be blasted.
     :param file_format: The format of the input_file.
     :param output_file: Where to store the results.
@@ -75,19 +81,43 @@ def bio_blaster(
     return result_handle_bak
 
 
-large_job = False
-if __name__ == '__main__':
-    time.sleep(0.4)  # post no more than three URL requests per second
-    if large_job:  # Limit large jobs to either weekends or between 9:00 PM and 5:00 AM Eastern time during weekdays.
-        job_time = time.gmtime(time.time())
-        if not job_time[6] < 5 and not job_time[3] < 21:
-            # noinspection SpellCheckingInspection
-            data_bak = bio_blaster(
-                input_file="/home/s_k_tiger/Downloads/Course4_dataset_v04_mod.fastq", file_format="fastq",
-                output_file="/home/s_k_tiger/Downloads/my_blast.xml",
-                index='HWI-M02942:21:000000000-ACNW4:1:1101:19810:2642_1'
+def biopython_use(
+        result_loc: str, get_data=False, large_job=True, input_file: str = None, file_format: str = None,
+        index: str = None, print_results=False, e_value_thresh=0.04
+):
+    """Getting and/or printing results.
+    :param result_loc: Where to store/get the results.
+    :param get_data: Whether to blast or not.
+    :param large_job: Check whether you are allowed to do large jobs.
+    :param input_file: The file path which file contains a/the sequence that is to be blasted.
+    :param file_format: The format of the input_file.
+    :param index: If the file contains multiple seqs and you want to analise 1 place its ref code here.
+    :param print_results: Whether to print the results.
+    :param e_value_thresh: if print_results is True; Threshold of e_value above which the results are not printed.
+    :return: None.
+    """
+    if get_data:
+        # https://www.ncbi.nlm.nih.gov/books/NBK25497/#chapter2.Usage_Guidelines_and_Requiremen
+        from time import time, sleep, gmtime
+        sleep(0.4)  # post no more than three URL requests per second
+        if large_job:  # Limit large jobs to either weekends or in between 9:00 PM to 5:00 AM (EST).
+            job_time = gmtime(time())
+            if not job_time[6] < 5 and not job_time[3] < 21:
+                # noinspection SpellCheckingInspection
+                bio_blaster(
+                    input_file=input_file, file_format=file_format,
+                    output_file=result_loc,
+                    index=index
+                )
+            del job_time
+        else:
+            bio_blaster(
+                input_file=input_file, file_format=file_format,
+                output_file=result_loc,
+                index=index
             )
-        del job_time
-    results = open("/home/s_k_tiger/Downloads/my_blast.xml")
+    results = open(result_loc)
     blast_records = NCBIXML.parse(results)
-    data_printer(blast_record=blast_records, e_value_thresh=0.04)
+    if print_results:
+        data_printer(blast_record=blast_records, e_value_thresh=e_value_thresh)
+    return
