21010565e813b430ead55c01d001a5ac24db37f0
CommanderKronos <yuriwit0@gmail.com>
2020-05-27 13:17:04 +0200

    Fixed navigations

--- application.py
+++ application.py
@@ -5,7 +5,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def website():
-    return render_template("../blok4afvink2_2/templates/project.html")
+    return render_template("project.html")
+
+
+@app.route('/project.html')
+def website_home():
+    return render_template("project.html")
+
+
+@app.route('/over ons.html')
+def site_over_ons():
+    return render_template("over ons.html")
+
+
+@app.route('/database.html')
+def site_database():
+    return render_template("database.html")
 
 
 if __name__ == '__main__':
